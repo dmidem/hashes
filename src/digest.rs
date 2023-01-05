b2ae1c90649d21b417c307c235c311dab09ef027
Dmitry Demin <dmitry.demin@outlook.com>
2023-01-05 10:24:33 +0100

    Change Digest to store inner as bytes

--- src/digest.rs
+++ src/digest.rs
@@ -1,93 +1,59 @@
 macro_rules! define_digest {
     () => {
         #[derive(PartialEq, Eq, Clone, Copy, Hash)]
-        pub struct Digest([Word; N_DIGEST_WORDS]);
+        pub struct Digest<const N_BYTES: usize>([u8; N_BYTES]);
 
-        impl Digest {
-            pub const N_WORD_BYTES: usize = std::mem::size_of::<Word>();
-            pub const N_DIGEST_BYTES: usize = N_DIGEST_WORDS * Self::N_WORD_BYTES;
-
-            pub fn into_words(self) -> [Word; N_DIGEST_WORDS] {
-                self.0
-            }
-
-            pub fn from_words(words: [Word; N_DIGEST_WORDS]) -> Self {
-                Self(words)
-            }
-
-            pub fn into_bytes(self) -> [u8; Self::N_DIGEST_BYTES] {
-                let mut bytes = [0u8; Self::N_DIGEST_BYTES];
-
-                self.0
-                    .map(|d| d.to_be_bytes())
-                    .into_iter()
-                    .zip(bytes.chunks_mut(Self::N_WORD_BYTES))
-                    .for_each(|(d, b)| b.copy_from_slice(&d));
-
-                bytes
-            }
-
+        impl<const N_BYTES: usize> Digest<N_BYTES> {
             pub fn from_bytes(bytes: [u8; Self::N_DIGEST_BYTES]) -> Self {
-                let mut words = [0; N_DIGEST_WORDS];
+                Self(bytes)
+            }
 
-                words
-                    .iter_mut()
-                    .zip(
-                        bytes
-                            .chunks(Self::N_WORD_BYTES)
-                            .map(|chunk| Word::from_be_bytes(chunk.try_into().unwrap())),
-                    )
-                    .for_each(|(w, b)| *w = b);
-
-                Self(words)
+            pub fn into_bytes(self) -> [u8; N_BYTES] {
+                self.0
             }
         }
 
-        impl std::fmt::Display for Digest {
+        impl<const N_BYTES: usize> std::fmt::Display for Digest<N_BYTES> {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                for b in self.into_bytes() {
+                for b in self.0 {
                     write!(f, "{:02x}", b)?
                 }
                 Ok(())
             }
         }
 
-        impl std::fmt::Debug for Digest {
+        impl<const N_BYTES: usize> std::fmt::Debug for Digest<N_BYTES> {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
                 write!(f, "\"{}\"", self)
             }
         }
 
-        impl std::str::FromStr for Digest {
+        impl<const N_BYTES: usize> std::str::FromStr for Digest<N_BYTES> {
             type Err = String;
 
             fn from_str(s: &str) -> Result<Self, Self::Err> {
-                let mut words = [0; N_DIGEST_WORDS];
+                let mut bytes = [0; N_BYTES];
 
-                if s.len() != N_DIGEST_WORDS * 2 * Self::N_WORD_BYTES {
+                if s.len() != 2 * N_BYTES {
                     return Err("Invalid digest string length".into());
                 }
 
-                words
+                bytes
                     .iter_mut()
-                    .zip(
-                        s.as_bytes()
-                            .chunks(2 * Self::N_WORD_BYTES)
-                            .map(std::str::from_utf8),
-                    )
-                    .try_for_each(|(word, chunk)| {
+                    .zip(s.as_bytes().chunks(2).map(std::str::from_utf8))
+                    .try_for_each(|(byte, chunk)| {
                         chunk
                             .map_err(|_| "Invalid utf8 character in digest string".to_string())
                             .and_then(|chunk| {
-                                Word::from_str_radix(chunk, 16)
+                                u8::from_str_radix(chunk, 16)
                                     .map_err(|_| "Invalid hex character in digest string".into())
                                     .map(|value| {
-                                        *word = value;
+                                        *byte = value;
                                     })
                             })
                     })?;
 
-                Ok(Self(words))
+                Ok(Self(bytes))
             }
         }
     };
--- src/tests.rs
+++ src/tests.rs
@@ -1,38 +1,14 @@
-macro_rules! define_digest_convert_tests {
-    ($words:expr, $bytes:expr) => {
-        #[test]
-        fn test_digest_convert() {
-            let words = $words;
-            let bytes = $bytes;
-
-            assert_eq!(Digest::from_words(words).into_bytes(), bytes);
-            assert_eq!(Digest::from_bytes(bytes).into_words(), words);
-
-            assert_eq!(
-                Digest::from_words(words)
-                    .to_string()
-                    .parse::<Digest>()
-                    .unwrap()
-                    .into_words(),
-                words
-            );
-        }
-    };
-}
-
-pub(super) use define_digest_convert_tests;
-
 macro_rules! define_hash_tests {
     ($small_data_results:expr, $big_data_result:expr) => {
         #[test]
         fn test_small_data() {
-            assert_eq!(hash("".as_bytes()).into_words(), $small_data_results[0]);
+            assert_eq!(hash("".as_bytes()).into_bytes(), $small_data_results[0]);
 
-            assert_eq!(hash("abc".as_bytes()).into_words(), $small_data_results[1]);
+            assert_eq!(hash("abc".as_bytes()).into_bytes(), $small_data_results[1]);
 
             assert_eq!(
                 hash("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq".as_bytes())
-                    .into_words(),
+                    .into_bytes(),
                 $small_data_results[2]
             );
 
@@ -42,12 +18,12 @@ macro_rules! define_hash_tests {
                     hijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu"
                         .as_bytes()
                 )
-                .into_words(),
+                .into_bytes(),
                 $small_data_results[3]
             );
 
             assert_eq!(
-                hash(&vec![0x61u8; 1000000]).into_words(),
+                hash(&vec![0x61u8; 1000000]).into_bytes(),
                 $small_data_results[4]
             );
         }
@@ -61,7 +37,7 @@ macro_rules! define_hash_tests {
                         .repeat(16_777_216)
                         .as_bytes()
                 )
-                .into_words(),
+                .into_bytes(),
                 $big_data_result
             );
         }
